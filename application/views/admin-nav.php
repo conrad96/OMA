a91ccea737ed0586fdedb5c18bde0b03873c6cb6
conrad96 <conrad.mugisha@gmail.com>
2018-08-24 10:55:29 +0300

    styling header and included logo

--- application/views/admin-nav.php
+++ application/views/admin-nav.php
@@ -1,4 +1,5 @@
 <?php include("shared/header.php"); ?>
+<div class="container-fluid">
 <ul class="nav nav-pills">
 <li class='active'><a href='<?php echo $assets["base_url"]."Market/admin/".$id."/".$name; ?>'><span class='fa fa-user'></span><?php echo $name; ?></a></li>
 <li class='pull-right'><a href='<?php echo $assets['base_url']."market/logout"; ?>'><span class='fa fa-sign-out'></span>Logout</a></li>
--- application/views/farmer-nav.php
+++ application/views/farmer-nav.php
@@ -1,5 +1,5 @@
 <?php include("shared/header.php"); ?>
-
+<div class="container-fluid">
 <ul class="nav nav-pills">
 <li class='active'><a href=<?php echo $assets['base_url']."market/farmer/".$id."/".$name; ?>><span class='fa fa-user'></span><?php echo $name; ?></a></li>
 <li class='pull-right'><a href='<?php echo $assets['base_url']."market/logout"; ?>'><span class='fa fa-sign-out'></span>Logout</a></li>
